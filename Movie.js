bc5661e13027e5877314097632c9bb893a3010cd
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-08 17:36:42 -0400

    jquery and loading

---
+++ Movie.js
@@ -0,0 +1,7 @@
+const url = 'https://light-flax-icebreaker.glitch.me/';
+
+$(document).ready(function() {
+    // Display a "loading..." message
+    $('#loading').show();
+    $('#movies').hide();
+});
