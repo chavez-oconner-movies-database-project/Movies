10dc9aad1cbc042896f9dbd682ea0ab2021e351b
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-09 16:59:12 -0400

    edit function finally updates page.

--- Movie.js
+++ Movie.js
@@ -87,11 +87,11 @@
 //         }
 //     });
 
-(function() {
+(function () {
     "use strict";
     const url = "https://light-flax-icebreaker.glitch.me/movies";
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         // Display a "loading..." message
         $("#loading").show();
         $("#movies").hide();
@@ -126,14 +126,14 @@
                     $("#movies").html(html);
 
                     // Add event listeners for edit and delete buttons
-                    $(".edit-movie-btn").click(function() {
+                    $(".edit-movie-btn").click(function () {
                         var movieId = $(this).data("id");
 
                         // Make an AJAX request to get the movie details
                         $.ajax({
                             method: "GET",
                             url: url + "/" + movieId,
-                            success: function(data) {
+                            success: function (data) {
                                 // Populate the edit form with the movie details
                                 $("#edit-movie-form input[name='id']").val(data.id);
                                 $("#edit-movie-form input[name='title']").val(data.title);
@@ -142,13 +142,13 @@
                                 // Show the edit form
                                 // $('#edit-movie-modal').modal('show');
                             },
-                            error: function() {
+                            error: function () {
                                 alert("Error fetching movie details");
                             },
                         });
                     });
 
-                    $(".delete-movie-btn").click(function() {
+                    $(".delete-movie-btn").click(function () {
                         console.log("delete btn clicked");
                         var movieId = $(this).data("id");
                         console.log(movieId);
@@ -159,14 +159,14 @@
                             $.ajax({
                                 method: "DELETE",
                                 url: url + "/" + movieId,
-                                success: function() {
+                                success: function () {
                                     // Remove the movie from the UI
                                     $(".movie[data-id='" + movieId + "']").remove();
                                     console.log("success triggered... should be deleted");
                                     // Render all movies again
                                     getMovies();
                                 },
-                                error: function() {
+                                error: function () {
                                     alert("Error deleting movie");
                                 },
                             });
@@ -180,105 +180,96 @@
 
         // Call the function to get and render all movies
         getMovies();
-    function addMovie(title, rating) {
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ title, rating }),
-        })
-            .then(() => {
-                // Clear the form
-                $("#add-movie-form")[0].reset();
+        // function addMovie(title, rating) {
+        //     fetch(url, {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify({ title, rating }),
+        //     })
+        //         .then(() => {
+        //             // Clear the form
+        //             $("#add-movie-form")[0].reset();
+        //
+        //             // Hide the add movie modal
+        //             // $('#add-movie-modal').modal('hide');
+        //
+        //             // Get and render all movies
+        //             getMovies();
+        //         })
+        //         .catch(() => {
+        //             alert("Error adding movie");
+        //         });
+        // }
 
-                // Hide the add movie modal
-                // $('#add-movie-modal').modal('hide');
+        // function editMovie(id, title, rating) {
+        //     fetch(url + "/" + id, {
+        //         method: "PUT",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify({ title, rating }),
+        //     })
+        //         .then(() => {
+        //             // Clear the form
+        //             $("#edit-movie-form")[0].reset();
+        //
+        //             // Hide the edit movie modal
+        //             // $('#edit-movie-modal').modal('hide');
+        //
+        //             // Get and render all movies
+        //             getMovies();
+        //         })
+        //         .catch(() => {
+        //             alert("Error editing movie");
+        //         });
+        // }
+        // });
 
-                // Get and render all movies
-                getMovies();
-            })
-            .catch(() => {
-                alert("Error adding movie");
+
+        // Handle submit event for edit movie form
+        $('#edit-movie-form').click(function (event) {
+            event.preventDefault();
+
+            var movieId = $('#edit-movie-form input[name="id"]').val();
+            var movieTitle = $('#edit-movie-form input[name="title"]').val();
+            var movieRating = $('#edit-movie-form input[name="rating"]').val();
+
+            // Make an AJAX request to update the movie
+            $.ajax({
+                method: 'PUT',
+                url: url + '/' + movieId,
+                data: {
+                    title: movieTitle,
+                    rating: movieRating
+                },
+                success: function (data) {
+                    // Fetch the updated movie data from the server
+                    $.ajax({
+                        method: 'GET',
+                        url: url + '/' + movieId,
+                        success: function (movie) {
+                            // Update the movie in the UI with the new data
+                            $('.movie[data-id="' + movieId + '"] h2').text(movie.title);
+                            $('.movie[data-id="' + movieId + '"] .rating').text(movie.rating);
+                            $('.movie[data-id="' + movieId + '"] .director').text(movie.director);
+                            $('.movie[data-id="' + movieId + '"] .genre').text(movie.genre);
+
+                            var newEdit = $('<div class="movie" data-id="' + movie.id + '">');
+                            newEdit.html('<h2>' + movie.title + '</h2>');
+                            newEdit.append('<div class="rating">' + movie.rating + '</div>');
+                            newEdit.append('<div class="director">' + movie.director + '</div>');
+                            newEdit.append('<div class="genre">' + movie.genre + '</div>');
+
+                            // Add the new movie element to the movie list on the page
+                            $('#movies').append(newEdit);
+                            getMovies();
+                        }
+                    });
+                }
             });
-    }
-
-    function editMovie(id, title, rating) {
-        fetch(url + "/" + id, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ title, rating }),
-        })
-            .then(() => {
-                // Clear the form
-                $("#edit-movie-form")[0].reset();
-
-                // Hide the edit movie modal
-                // $('#edit-movie-modal').modal('hide');
-
-                // Get and render all movies
-                getMovies();
-            })
-            .catch(() => {
-                alert("Error editing movie");
-            });
-    }
-    });
-
-// Function to fetch movie data from server
-//     function getMovies(movieId, callback) {
-//         $.ajax({
-//             method: 'GET',
-//             url: url + '/' + movieId,
-//             success: function (movie) {
-//                 callback(movie);
-//             }
-//         });
-//     }
-
-//     // Handle submit event for edit movie form
-//     $('#edit-movie-form').click(function (event) {
-//         event.preventDefault();
-//
-//         var movieId = $('#edit-movie-form input[name="id"]').val();
-//         var movieTitle = $('#edit-movie-form input[name="title"]').val();
-//         var movieRating = $('#edit-movie-form input[name="rating"]').val();
-//
-//         // Make an AJAX request to update the movie
-//         $.ajax({
-//             method: 'PUT',
-//             url: url + '/' + movieId,
-//             data: {
-//                 title: movieTitle,
-//                 rating: movieRating
-//             },
-//             success: function (data) {
-//                 // Fetch the updated movie data from the server
-//                 $.ajax({
-//                     method: 'GET',
-//                     url: url + '/' + movieId,
-//                     success: function (movie) {
-//                         // Update the movie in the UI with the new data
-//                         $('.movie[data-id="' + movieId + '"] h2').text(movie.title);
-//                         $('.movie[data-id="' + movieId + '"] .rating').text(movie.rating);
-//                         $('.movie[data-id="' + movieId + '"] .director').text(movie.director);
-//                         $('.movie[data-id="' + movieId + '"] .genre').text(movie.genre);
-//
-//                         var newEdit = $('<div class="movie" data-id="' + movie.id + '">');
-//                         newEdit.replace('<h2>' + movie.title + '</h2>');
-//                         newEdit.replace('<div class="rating">' + movie.rating + '</div>');
-//                         newEdit.replace('<div class="director">' + movie.director + '</div>');
-//                         newEdit.replace('<div class="genre">' + movie.genre + '</div>');
-//
-//                         // Add the new movie element to the movie list on the page
-//                         $('#movies').append(newEdit);
-//                     }
-//                 });
-//             }
-//         });
-//     });
+        });
 //     // allow users to add New Movies:
 //     const addMovie = document.getElementById('add-movie');
 //
@@ -320,4 +311,5 @@
 //             });
 //     });
 //
-})();
+    });
+    })();
